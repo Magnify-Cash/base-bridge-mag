ca18ae9b48cd8696d5e66e2ea4fa4cb15d4f575f
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-11-27 08:57:44 -0500

    configure oft / adapter scripts

--- backend/scripts/ConfigureAdapter.ts
+++ backend/scripts/ConfigureAdapter.ts
@@ -8,24 +8,25 @@ const hre = require('hardhat')
 async function main() {
     // Address of the deployed contracts
     const oftAddress = '0xd70212BA8531a642079c477D976A4A27a0C8A708' // Replace with actual address
-    const oftAdapter = '0x14378084BA3Fa1CdB032836AdBA1517154c52CF7' // Replace with actual address
+    const adapterAddress = '0x14378084BA3Fa1CdB032836AdBA1517154c52CF7' // Replace with actual address
 
     // Endpoint IDs for the chains
-    const aEid = EndpointId.SEPOLIA_V2_TESTNET
+    const bEid = EndpointId.BASESEP_V2_TESTNET
 
     // Get the contract factories
-    const MagOFT = await hre.ethers.getContractFactory('MagOFT')
+    const MagOFTAdapter = await hre.ethers.getContractFactory('MagOFTAdapter')
 
-    // Attach to the deployed contracts
-    const oft = MagOFT.attach(oftAddress)
+    // Attach to the deployed contract
+    const oftAdapter = MagOFTAdapter.attach(adapterAddress)
 
     // Convert addresses to bytes32
-    const bytes32Adapter = pad(oftAdapter, { size: 32 })
+    const bytes32OFT = pad(`${oftAddress}`, { size: 32 })
+    console.log(bytes32OFT)
 
-    // Transaction to set peer for OFT
-    const setOFTPeerTx = await oft.setPeer(aEid, bytes32Adapter)
-    await setOFTPeerTx.wait()
-    console.log('OFT peer set successfully')
+    // Transaction to set peer for Adapter
+    const setAdapterPeerTx = await oftAdapter.setPeer(bEid, bytes32OFT)
+    await setAdapterPeerTx.wait()
+    console.log('Adapter peer set successfully')
 }
 
 // We recommend this pattern to be able to use async/await everywhere
