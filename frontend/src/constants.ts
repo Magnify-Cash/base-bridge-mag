5e2b381f070741e51c31720353704d257f69bfca
kvn <42082827+crypto-rizzo@users.noreply.github.com>
2024-11-27 23:11:40 -0500

    configure frontend

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,12 +1,12 @@
 import { EndpointId } from "@layerzerolabs/lz-definitions";
 
-export const SOURCE_CHAIN = 11155111;
-export const DESTINATION_CHAIN = 84532;
-export const BRIDGE_ADDRESS = "0x14378084BA3Fa1CdB032836AdBA1517154c52CF7";
+export const SOURCE_CHAIN = 1;
+export const DESTINATION_CHAIN = 8453;
+export const BRIDGE_ADDRESS = "0xB5c6D23c2a29E40fe8C79AcfED435cb48AF02d68";
 export const SOURCE_TOKEN_ADDRESS =
-  "0x341DB579E6A62831d608Cbfa5aCc6349579Ba0f1";
+  "0x71DA932ccdA723BA3ab730C976bC66DaAF9C598c";
 export const DESTINATION_TOKEN_ADDRESS =
-  "0xd70212BA8531a642079c477D976A4A27a0C8A708";
+  "0x59F680F431f5280e7662b96F2DFA195D1693852d";
 export const LZ_OPTIONS = "0x0003010011010000000000000000000000000000ea60";
 export const getEid = (chainId: number) => {
   switch (chainId) {
@@ -15,9 +15,9 @@ export const getEid = (chainId: number) => {
     case 84532:
       return EndpointId.BASESEP_V2_TESTNET;
     case 8453:
-      return EndpointId.ETHEREUM_V2_MAINNET;
-    case 1:
       return EndpointId.BASE_V2_MAINNET;
+    case 1:
+      return EndpointId.ETHEREUM_V2_MAINNET;
     default:
       return 0;
   }
